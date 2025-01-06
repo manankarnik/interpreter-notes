c376e8c369b4de7ad6725274bf6edf8700d0aee7
Manan Karnik <manankarnik@gmail.com>
2025-01-06 22:25:07 +0530

    Implement interpret_file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-use std::{io, io::Write, env};
+use std::{
+    env,
+    io::{self, Write},
+    path, process,
+};
 
-fn main() {
-    let args = env::args();
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         repl();
+    } else if args.len() == 2 {
+        interpret_file(&args[1])?;
     } else {
-        todo!()
+        let program = path::Path::new(args[0].as_str())
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        eprintln!("Usage: {program} [file]");
+        process::exit(64);
     }
+    Ok(())
+}
+
+fn interpret(line: &str) {
+    let tokens: Vec<&str> = line.split(" ").collect();
+    for token in tokens {
+        println!("{token}");
+    }
+}
+
+fn interpret_file(file_name: &str) -> io::Result<()> {
+    let lines = std::fs::read_to_string(file_name)?;
+    let lines: Vec<&str> = lines.split("\n").collect();
+    for line in lines {
+        interpret(&line);
+    }
+    Ok(())
 }
 
 fn repl() {
@@ -14,10 +43,13 @@ fn repl() {
         print!("> ");
         io::stdout().flush().expect("Failed to flush stdout");
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input");
-        match input.as_str() {
-            "exit\n" => return,
-            value    => print!("{value}"),
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => break,
+            Ok(_) => interpret(&input),
+            Err(e) => {
+                eprintln!("Error reading input: {e}");
+                break;
+            }
         }
     }
 }
